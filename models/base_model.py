851e5dc503d0dd0c440fe37060c5959c5956f38a
Yahya Achrane <yahya.achrane15@gmail.com>
2023-10-10 05:45:48 -0700

    initialising a base object

--- models/base_model.py
+++ models/base_model.py
@@ -9,4 +9,25 @@ import models
 
 
 class BaseModel:
-    pass
+    """
+
+    """
+    def __init__(self, *args, **kwargs):
+        '''
+        Docs
+        '''
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at':
+                    value = datetime.fromisoformat(value)
+                    self.created_at = value
+                elif key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                    self.updated_at = value
+                elif key == 'id':
+                    self.id = str(value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
