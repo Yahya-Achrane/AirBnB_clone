7dbb574c83d39a290d2f7460275be3e1535b808a
Yahya Achrane <yahya.achrane15@gmail.com>
2023-10-13 07:07:08 -0700

    update

--- console.py
+++ console.py
@@ -93,7 +93,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_all(self, arg):
         """
-        Docs
+        empty for now
         """
         if len(arg) > 0:
             args_array = arg.split()
@@ -111,6 +111,35 @@ class HBNBCommand(cmd.Cmd):
             for key, value in models.storage.all().items():
                 final_list.append(str(value))
             print(final_list)
+
+    def do_update(self, arg):
+        """
+        update docs ..
+        """
+        if len(arg) > 0:
+            args_array = arg.split()
+            if len(args_array) > 0:
+                if args_array[0] == "BaseModel":
+                    if len(args_array) > 1:
+                        objs_dict = models.storage.all()
+                        search_string = "BaseModel.{}".format(args_array[1])
+                        if search_string in objs_dict:
+                            if len(args_array) > 2:
+                                if len(args_array) > 3:
+                                    if (args_array[3] not in ["created_at", "updated_at", "id"]):
+                                        setattr(objs_dict[search_string], str(args_array[2]), str(args_array[3]))
+                                else:
+                                    print("** value missing **")
+                            else:
+                                print("** attribute name missing **")
+                        else:
+                            print("** no instance found **")
+                    else:
+                        print("** instance id missing **")
+                else:
+                    print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
