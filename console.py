57f83b7a80acd0033768056607130374db77f4ee
Abdallah BENJRADA <abdallah.benjrada@gmail.com>
2023-10-15 22:39:53 +0200

    Update do_update

--- console.py
+++ console.py
@@ -164,7 +164,11 @@ class HBNBCommand(cmd.Cmd):
 
     def do_update(self, arg):
         """
-        update docs ..
+        Usage: update <class> <id> <attribute_name> <attribute_value> or
+       <class>.update(<id>, <attribute_name>, <attribute_value>) or
+       <class>.update(<id>, <dictionary>)
+        Update a class instance of a given id by adding or updating
+        a given attribute key/value pair or dictionary.
         """
         allowed_classes = ["BaseModel", "User", "State",
                            "City", "Amenity", "Place", "Review"]
